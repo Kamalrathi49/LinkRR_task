0f540bd52f31e46261c32e0389454f4c8d2b02f9
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-07 22:18:10 +0530

    deploying react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaSpotify,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <img
+        className="avatar"
+        src="https://linkrr.in/uploads/avatars/13eb41f6a112c6773427fc24b53d06b1.png"
+        alt="Avatar"
+      />
+      <div id="title">
+        <h1 className="title">DESIGNSUBWAY</h1>
+        <p className="sub-title">GRAPHIC DESIGNER</p>
+      </div>
+      <div id="button">
+        <button
+          className="button"
+          style={{ backgroundColor: "#fff", color: "#000" }}
         >
-          Learn React
-        </a>
-      </header>
+          Photoshop Course
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#ff0000", color: "#FFFFFF" }}
+        >
+          YOUTUBE
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#0D0D0D", color: "#FFF7F7" }}
+        >
+          SUPPORT ME
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#0095FF", color: "#FFFFFF" }}
+        >
+          TELEGRAM CHANNEL
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#003638", color: "#FFFFFF" }}
+        >
+          HOW TO GROW YOUR INSTAGRAM
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#D9FF38", color: "#000" }}
+        >
+          RECOMMENDED LAPTOPS
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#00FF00", color: "#FFFEFE" }}
+        >
+          HIRE ME ON FIVERR{" "}
+        </button>
+        <button
+          className="button"
+          style={{ backgroundColor: "#00C343", color: "#FFFFFF" }}
+        >
+          WHATSAPP GROUP
+        </button>
+      </div>
+      <div className="contact-menu">
+        <FaEnvelope className="contact-icon" />
+        <FaFacebook className="contact-icon" />
+        <FaInstagram className="contact-icon" />
+        <FaTwitter className="contact-icon" />
+        <FaYoutube className="contact-icon" />
+        <FaLinkedin className="contact-icon" />
+        <FaSpotify className="contact-icon" />
+      </div>
+      <p className="footer">Designsubway</p>
     </div>
   );
-}
+};
 
 export default App;
